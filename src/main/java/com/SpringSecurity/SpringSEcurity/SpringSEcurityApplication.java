3d1694f1ba45ec61c15d0e16916623056b715dd6
Prithwish Pramanik <prithwishpramanik462@gmail.com>
2025-03-27 10:02:50 +0530

    initial Spring Security setup

---
+++ src/main/java/com/SpringSecurity/SpringSEcurity/SpringSEcurityApplication.java
@@ -0,0 +1,13 @@
+package com.SpringSecurity.SpringSEcurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSEcurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSEcurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringSecurity/SpringSEcurity/SpringSEcurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringSecurity.SpringSEcurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSEcurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
